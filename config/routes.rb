2a3fde10bab49579cc04b8c98d15bfb711cb2ad5
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-11-09 09:31:41 +0000

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,35 @@
 Rails.application.routes.draw do
-  get 'tags/show'
   devise_for :users
+  # get 'tags/show'
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'posts#index'
+  # root to: 'posts#index'
   resources :hellos
-  resources :brands, only: :create
-  resources :posts do
-    resources :comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
-  end
-  get 'others' => 'posts#others'
-  resources :nominees do
-    get 'posts', to: 'posts#search'
-  end
-  resources :users do
-    resources :jeans
-    resource :relationships, only: [:create, :destroy]
-    get :followings, on: :member
-    get :followers, on: :member
-  end
-  get 'my_page' => 'users#my_page'
-  get 'my_page/edit' => 'users#edit'
-  get 'my_page/jeans' => 'jeans#index'
-  get 'my_page/jeans/:id' => 'jeans#show', as: 'my_jeans'
-  get 'my_page/timeline' => 'posts#timeline', as: 'timeline'
-  get 'this_week', to: 'nominees#this_week'
-  get 'last_weeks_winner', to: 'nominees#last_weeks_winner'
-  resources :tags do
-    get 'posts', to: 'posts#search'
-  end
-  resources :nominee_maps, only: [:create, :destroy]
+  # resources :brands, only: :create
+  # resources :posts do
+  #   resources :comments, only: [:create, :destroy]
+  #   resource :favorites, only: [:create, :destroy]
+  # end
+  # get 'others' => 'posts#others'
+  # resources :nominees do
+  #   get 'posts', to: 'posts#search'
+  # end
+  # resources :users do
+  #   resources :jeans
+  #   resource :relationships, only: [:create, :destroy]
+  #   get :followings, on: :member
+  #   get :followers, on: :member
+  # end
+  # get 'my_page' => 'users#my_page'
+  # get 'my_page/edit' => 'users#edit'
+  # get 'my_page/jeans' => 'jeans#index'
+  # get 'my_page/jeans/:id' => 'jeans#show', as: 'my_jeans'
+  # get 'my_page/timeline' => 'posts#timeline', as: 'timeline'
+  # get 'this_week', to: 'nominees#this_week'
+  # get 'last_weeks_winner', to: 'nominees#last_weeks_winner'
+  # resources :tags do
+  #   get 'posts', to: 'posts#search'
+  # end
+  # resources :nominee_maps, only: [:create, :destroy]
 
 end
