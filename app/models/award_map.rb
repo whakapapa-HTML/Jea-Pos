75a802e7b9b025f1f9e1c2660fb1922589303508
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-11-09 12:53:07 +0000

    [Add] award_maps

---
+++ app/models/award_map.rb
@@ -0,0 +1,8 @@
+class AwardMap < ApplicationRecord
+
+  belongs_to :jeans, optional: true
+  belongs_to :award, optional: true
+
+  enum award_id: { "Best Faded": 1, "Best Fashionable": 2, "Best Whisker": 3, "Best Shevron": 4, "Best 501": 5 }
+
+end
---
+++ db/migrate/20211109125009_create_award_maps.rb
@@ -0,0 +1,10 @@
+class CreateAwardMaps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :award_maps do |t|
+      t.integer :award_id
+      t.integer :jeans_id
+
+      t.timestamps
+    end
+  end
+end
