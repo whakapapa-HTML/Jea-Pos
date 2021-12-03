761907efff5f10a412597699a74a61e241cb4f90
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-12-03 16:25:03 +0900

    [Edit] top image

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
+    
   end
 
 
