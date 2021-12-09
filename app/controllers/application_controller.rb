4b33728195ce4c642275a21fb1f1a218cff421d7
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-12-09 19:57:40 +0900

    [Edit] ログイン後のリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, unless: :devise_or_home_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
 
   protected
 
@@ -11,4 +14,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     others_path # ログアウト後に遷移するpathを設定
   end
+
+  private
+    # ログイン前にクリックした詳細ページのパスを保存する
+    def store_current_location
+      store_location_for(:user, request.url)
+    end
+
+    def devise_or_home_controller?
+      devise_controller? || others_path
+    end
 end
